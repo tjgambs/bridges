cb85e1fda120d4cc19d507e91883ecfff1943624
Tim <tjgambs@gmail.com>
2018-02-24 14:33:09 -0600

    Refactor and added distance

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var markers = {};
 var selectedIndustries = [];
 var map;
 var homeMarker;
-
+var currentRadius = 1;
 
 function initAutocomplete() {
   var input = document.getElementById('AddressSearch');
@@ -32,7 +32,31 @@ function initAutocomplete() {
       } else {
           document.getElementById('AddressSearch').placeholder = 'Starting Address';
       }
+      for (var key in markers) {
+        if (markers.hasOwnProperty(key)) {           
+            for(var i = 0; i < markers[key].length; i++) {
+              var point = markers[key][i].position;
+              markers[key][i].distance = getDistance(point, homeMarker.position);
+            }
+        }
+      }
   }
+}
+
+function getDistance(p1, p2) {
+    function rad(x) {
+      return x * Math.PI / 180;
+    }
+    // Haversine distance formula
+    var R = 6378137; // Earth’s mean radius in meter
+    var dLat = rad(p2.lat() - p1.lat());
+    var dLong = rad(p2.lng() - p1.lng());
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
+    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d * 0.000621371; // returns the distance in miles
 }
 
 
@@ -40,11 +64,13 @@ function initMarkers() {
     var marker, i;
     var infowindow = new google.maps.InfoWindow();
     for (i = 0; i < data.length; i++) {
+        // Create the content for the popup window.
         var contentString = '<div><div>Name: ' + data[i][0] + '</div>' +
             '<div>Address: ' + data[i][4] + '</div>' +
             '<div>Industry: ' + data[i][1] + '</div>' +
             '<div>Position: ' + data[i][2] + '</div>' +
             '<div>Rate: $' + parseFloat(data[i][3]).toFixed(2) + '</div>' + '</div>';
+        // Create the marker that will be shown on the map.
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(data[i][5], data[i][6]),
             map: map,
@@ -53,22 +79,29 @@ function initMarkers() {
             contentString: contentString
         });
         marker.setVisible(false)
+        // Attach an event listener to each of the pins so when they are 
+        // clicked, we can get directions and display the info window.
         google.maps.event.addListener(marker, 'click', (function(marker) {
             return function() {
                 infowindow.setContent(marker.contentString);
                 infowindow.open(map, marker);
+
+                console.log('Go get directions!')
+
             }
         })(marker));
+        // Create a look up table for the markers based on industry.
         if (markers[data[i][1]] == undefined) {
-          markers[data[i][1]] = [marker]
+            markers[data[i][1]] = [marker]
         } else {
-          markers[data[i][1]].push(marker)
+            markers[data[i][1]].push(marker)
         }
     }
 }
 
 
 function initMap() {
+    // Initialize the map then initialize all of its dependencies.
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
         center: new google.maps.LatLng(41.7377, -87.6976)
@@ -79,17 +112,48 @@ function initMap() {
 
 
 function filterMarkers(category) {
+    // Check to see if this industry is already selected.
     var indexOfCategory = selectedIndustries.indexOf(category);
     if (indexOfCategory > -1) {
-      selectedIndustries.splice(indexOfCategory, 1)
-      for (var i = 0; i < markers[category.name].length; i++) {
-          markers[category.name][i].setVisible(false);
-      }
+        // If this industry is currently selected, then this function
+        // call is meant to remove all markers in this industry.
+        selectedIndustries.splice(indexOfCategory, 1)
+        for (var i = 0; i < markers[category.name].length; i++) {
+            markers[category.name][i].setVisible(false);
+        }
     } else {
-      selectedIndustries.push(category)
-      for (var i = 0; i < markers[category.name].length; i++) {
-          markers[category.name][i].setVisible(true);
-      }
+        // If this industry is not currently selected, then this function
+        // call is meant to add all markers in this indistry, within the 
+        // radius.
+        for (var i = 0; i < markers[category.name].length; i++) {
+            if (markers[category.name][i].distance > currentRadius) {
+                // If this location is greater than the selected radius
+                markers[category.name][i].setVisible(false);
+            } else {
+                // If this location is within the radius.
+                markers[category.name][i].setVisible(true);
+            }
+        }
+        selectedIndustries.push(category)
+    }
+}
+
+function updateRadius(radius) {
+    // Get the value of the selected radius and set it as the current radius.
+    var x = (radius.value || radius.options[radius.selectedIndex].value);
+    currentRadius = parseInt(x);
+    // Iterate through all of the selected categories and adjust according 
+    // to the new radius.
+    for (var key in selectedIndustries) {
+        if (markers.hasOwnProperty(key)) {           
+            for (var i = 0; i < markers[key].length; i++) {
+                if (markers[key][i].distance > currentRadius) {
+                    markers[key][i].setVisible(false);
+                } else {
+                    markers[key][i].setVisible(true);
+                }
+            }
+        }
     }
 }
 
